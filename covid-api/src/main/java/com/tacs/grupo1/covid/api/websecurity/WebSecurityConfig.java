f496fd5d3046211a2a154068ce31ac223a5c34ab
Santiago Varela <daronz@hotmail.com>
2021-08-24 23:35:46 -0300

    missing import

--- covid-api/src/main/java/com/tacs/grupo1/covid/api/websecurity/WebSecurityConfig.java
+++ covid-api/src/main/java/com/tacs/grupo1/covid/api/websecurity/WebSecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import javax.net.ssl.HttpsURLConnection;
 
 @Configuration
 @EnableWebSecurity
